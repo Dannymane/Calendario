85489137913c8675a1f80acd5062a85202d41cd4
Dannymane <daniel.yanko5@gmail.com>
2023-01-05 19:12:54 +0100

    Creating event class

---
+++ Event.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// class Event is a simple data transfer object, that is why all properties are public
+namespace Calendario.Images
+{
+    public class Event
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+    }
+}
